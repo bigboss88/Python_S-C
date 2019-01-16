383e79fcf154a5c330dad94de396ce7dce797b85
Jacob Shaw <jacob.shaw@unb.ca>
2019-01-15 20:08:00 -0400

    Added some very basic encoding

--- client.py
+++ client.py
@@ -1,11 +1,22 @@
 from socket import *
-target = input("Enter target ip addr: ")
-port = 14000
-addr = (target,port)
-UDPSock = socket(AF_INET,SOCK_DGRAM)
-while True:
-    data = input("Enter Message to send!: ")
-    UDPSock.sendto(str.encode(data),addr)
-    if data == "done":
-        break
-UDPSock.close
+def encrypt(string):
+	out =[]
+	[out.append(chr(ord(c)+12)) for c in string]
+	return "".join(out)
+
+def main():
+	target = input("Enter target ip addr: ")
+	port = 14000
+	addr = (target,port)
+	UDPSock = socket(AF_INET,SOCK_DGRAM)
+	key = "Aggnog"
+	while True:
+		data = input("Enter Message to send!: ")
+		data = encrypt(data)
+		UDPSock.sendto(data.encode(),addr)
+		if data == "done":
+			break
+	sUDPSock.close
+
+main()
+
